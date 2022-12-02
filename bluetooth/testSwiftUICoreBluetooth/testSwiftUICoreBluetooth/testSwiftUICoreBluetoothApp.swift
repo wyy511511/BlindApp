2fb0f59984d8ebf25a7fbeadb857cea531206096
wyy511511 <wuyaoyao0309@163.com>
2022-12-02 00:41:03 -0700

    backup Dec2

---
+++ bluetooth/testSwiftUICoreBluetooth/testSwiftUICoreBluetooth/testSwiftUICoreBluetoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  testSwiftUICoreBluetoothApp.swift
+//  testSwiftUICoreBluetooth
+//
+//  Created by 511 on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct testSwiftUICoreBluetoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
